6bc270fa2cbd8aba33d6cd7f9723f3b584effdda
Roman <dubrovin.skfo@gmail.com>
2022-12-30 21:37:03 +0300

    working with variables

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello world!!!')
+const hourlyRate = 80;
+const durationOfWork = 40;
+const numberDaysBeforeDeparture = 11 - 2;
+let numberHoursForWork = numberDaysBeforeDeparture * 5;
+
+console.log('Успею ли я выполнить работу (40 часов): ' + (numberHoursForWork >= durationOfWork));
+console.log('Стоимость 40 часовой работы: ' + (durationOfWork * hourlyRate) + '$');
