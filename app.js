4d69079e61ff5d9db9a9196461e87536a73577ec
Roman <dubrovin.skfo@gmail.com>
2022-12-31 15:11:53 +0300

    7.8 Упражнение - Кредит на MacBook

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-// Упрощение функции
+// 7.8 Упражнение - Кредит на MacBook
 
-function toPower(num, power) {
-    const result = num ** power;
-    return result;
+const AGE = 25;
+const IS_HAVE_JOB = false;
+const OWN_MONEY = 1800;
+const COST_LAPTOP = 2000;
+
+function possibleLoan(age, isHaveJob) {
+    if (age > 24 && isHaveJob) {
+        return 500;
+    } else if (age > 24 && !isHaveJob) {
+        return 100;
+    }
+    return 0;
 }
 
-console.log(toPower(2, 3));
+function isCanBuyLaptop(age, isHaveJob, ownMoney, costLaptop) {
+    const totalMoney = ownMoney + possibleLoan(age, isHaveJob);
+    return totalMoney >= costLaptop;
+}
 
-const res = (num, power) => num ** power;
-
-console.log(res(2, 4));
+console.log(isCanBuyLaptop(AGE, IS_HAVE_JOB, OWN_MONEY, COST_LAPTOP));
