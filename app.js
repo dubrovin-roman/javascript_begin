3ad4d300a759e46d89e1d04615a886f357e7fe4e
Roman <dubrovin.skfo@gmail.com>
2023-01-03 21:54:57 +0300

    9.7 Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,10 +1,61 @@
-// 9.3 Упражнение - Цикл в обратном порядке
+/*
+9.7 Упражнение - Расчёт итогового баланса
 
-const arr = ['!', 'JS', 'люблю', 'Я'];
+Есть выгрузка операций пользователя
 
-const newArr = new Array();
-for (let i = arr.length - 1; i > -1; i--) {
-    newArr.push(arr[i]);
+const operations = [1000, -700, 300, -500, 10000];
+а так же начальный баланс в 100$. Необходимо сделать функции расчёта:
+
+Итогового баланса
+Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
+Расчёта среднего расхода и среднего дохода
+*/
+
+const balanсeUSD = 100;
+const operations = [1000, -1500, 300, -800, 10000];
+
+function calculationOfFinalBalance(balanсeUSD, operations) {
+    let tempBalance = balanсeUSD;
+    for (const element of operations) {
+        tempBalance += element;
+        checkingNegativeBalance(tempBalance);
+    }
+    console.log(`Итоговый баланс ${tempBalance}$`);
 }
 
-console.log(newArr.join(' '));
+function checkingNegativeBalance(balanсeUSD) {
+    if (balanсeUSD < 0)
+        console.log('Баланс отрицательный');
+}
+
+function averageExpenseAndIncome(operations) {
+    let countNegativeNumbers = 0;
+    let countPositiveNumbers = 0;
+    let sumNegativeNumbers = 0;
+    let sumPositiveNumbers = 0;
+
+    for (const element of operations) {
+        if (element > 0) {
+            countPositiveNumbers++;
+            sumPositiveNumbers += element;
+        } else if (element < 0) {
+            countNegativeNumbers++;
+            sumNegativeNumbers += element;
+        }
+    }
+
+    if (countNegativeNumbers > 0) {
+        console.log(`Средний расход: ${sumNegativeNumbers / countNegativeNumbers}`);
+    } else {
+        console.log('Раход за период отсутствует');
+    }
+
+    if (countPositiveNumbers > 0) {
+        console.log(`Средний доход: ${sumPositiveNumbers / countPositiveNumbers}`);
+    } else {
+        console.log('Доход за период отсутствует')
+    }
+}
+
+calculationOfFinalBalance(balanсeUSD, operations);
+averageExpenseAndIncome(operations);
