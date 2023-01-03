fc0c2b5479b5f38965e89edfabe1b6f5ea675f8c
Roman <dubrovin.skfo@gmail.com>
2023-01-04 00:01:18 +0300

    11.6 Упражнение - среднее значение

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 /*
-11.4 Упражнение - вывод изменений цен
+11.6 Упражнение - среднее значение
 */
 
-const prices = [[100, 200], [120, 100], [200, 350]];
+const arr = [1, 4, 4, 10];
 
-const result = prices
-    .map(arr => arr[1] - arr[0])
-    .filter(el => el > 0);
+const arrSum = arr.reduce((acc, el) => acc += el, 0);
 
-console.log(result);
+console.log(`Среднее значение: ${arrSum / arr.length}`);
