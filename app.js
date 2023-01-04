9feead309145133fde7297b9ea4426a4e852ca9e
Roman <dubrovin.skfo@gmail.com>
2023-01-04 22:27:58 +0300

    13.3 Упражнение - сортировка пользователей

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 /*
-12.8 Упражнение - маскировка карты
+13.3 Упражнение - сортировка пользователей
 */
 
-const card = '2342834503458353';
+const users = [
+    {name: 'Аня', age: 23},
+    {name: 'Петя', age: 40},
+    {name: 'Миша', age: 30},
+    {name: 'Вася', age: 28},
+    {name: 'Таня', age: 25}
+];
 
-function maskingCard(card) {
-    return card.slice(-4).padStart(16, '*');
-}
+const sortUsers = users.sort((a, b) => {
+    return a.age - b.age;
+});
 
-console.log(maskingCard(card));
+console.log(sortUsers);
