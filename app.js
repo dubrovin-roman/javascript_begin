6755cfe7a6675f8b893694a273451a0f9e85e9c1
Roman <dubrovin.skfo@gmail.com>
2023-01-05 22:18:12 +0300

    15.10 Упражнение - объект в объекте

--- app.js
+++ app.js
@@ -1,65 +1,48 @@
+'use strict';
+
 /*
-13.11 Упражнение - склад
+15.10 Упражнение - объект в объекте
 */
 
-const warehouse = {
-    goods: [],
-    findGoodById: function (id) {
-        const findGood = this.goods.find(good => good.id === id);
-        if (!findGood) {
-            console.log(`Товар с id ${id} отсутствует на складе.`);
-        }
-        return findGood; 
-    },
-    addGood: function (newGood) {
-        const findGood = this.goods.find(good => good.id === newGood.id);
-        if (findGood) {
-            console.log(`Товар с id ${newGood.id} уже существует на складе.`);
-            return;
-        }
-        this.goods.push(newGood);
-    },
-    getWeightKg: function () {
-        return this.goods.reduce((acc, good) => {
-            const weightKg = good?.weight?.kg;
-            if (weightKg) {
-                return acc += weightKg;
+const company = {
+    name: 'ООО Агро',
+    employees: [
+        {
+            name: 'Света',
+            getName: function () {
+                return this.name;
             }
-            return acc;
-        }, 0);
-    }
-};
-
-const car = {
-    id: 1,
-    weight: {
-        kg: 1000
+        },
+        {
+            name: 'Олег',
+            getName: function () {
+                return this.name;
+            }
+        },
+        {
+            name: 'Евлампий',
+            getName: function () {
+                return this.name;
+            }
+        }
+    ],
+    ceo: {
+        name: 'Вася',
+        getName: function () {
+            return this.name;
+        }
     },
-    brand: 'Ford'
-};
-
-const chair = {
-    id: 2,
-    weight: {
-        kg: 2
+    getCompanyName: function () {
+        return this.name;
+    },
+    getCEOName: function () {
+        return this.ceo.getName();
+    },
+    getEmployeesNames: function () {
+        return this.employees.map(emp => emp.getName());
     }
 };
 
-const paper = {
-    id: 3,
-    color: 'red'
-}
-
-warehouse.addGood(car);
-console.log(warehouse.goods);
-warehouse.addGood(chair);
-console.log(warehouse.goods);
-warehouse.addGood(paper);
-console.log(warehouse.goods);
-console.log('--------------------');
-console.log(warehouse.findGoodById(1));
-console.log(warehouse.findGoodById(2));
-console.log(warehouse.findGoodById(3));
-console.log(warehouse.findGoodById(4));
-console.log('--------------------');
-console.log(warehouse.getWeightKg());
+console.log(company.getCompanyName());
+console.log(company.getCEOName());
+console.log(company.getEmployeesNames());
