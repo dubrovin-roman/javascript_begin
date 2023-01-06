ffd4bcd2fe10d109422e6a527d8efae713b36eba
Roman <dubrovin.skfo@gmail.com>
2023-01-07 01:35:21 +0300

    17.11 Упражнение - сохранение JSON

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 'use strict';
 /*
-17.7 Упражнение - получение DOM элементов
+17.11 Упражнение - сохранение JSON
 */
 
-const el1 = document.querySelectorAll('.one')[0].innerText;
-console.log(el1);
+function submitForm() {
+	const input = document.querySelector('.input').value;
+	if (!input) {
+		return;
+	}
 
-const el2 = document.querySelectorAll('.one')[1].innerText;
-console.log(el2);
+    const textObj = {
+        id: 1,
+        text: input
+    };
+    const textJSON = JSON.stringify(textObj);
+    localStorage.setItem('text', textJSON);
+    console.log(localStorage.getItem('q'));
+	
+    document.querySelector('.panel').innerText = input;
+	document.querySelector('.input').value = '';
+	document.querySelector('.notification').classList.add('notification_active');
+	/*
+    document.querySelector('.notification').classList.remove('notification_hidden');
+	console.log(document.querySelector('.notification').getAttribute('class'));
+	document.querySelector('.notification').setAttribute('class', 'notification')
+	document.querySelector('.notification').setAttribute('key', 1);
+	document.querySelector('.notification').setAttribute('user-id', 1);
+	console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
+    */
+}
 
-const el3 = document.querySelector('#two').innerText;
-console.log(el3);
+function inputChanged(e) {
+	if (e.code == 'Enter') {
+		submitForm()
+	}
+}
 
-const elem3 = document.getElementById('two').innerText;
-console.log(elem3);
+const buttonClass = 'button';
+const buttonText = 'Кнопка';
 
-const el4 = document.querySelector('[user-id="4"]').innerText;
-console.log(el4);
+const newElement = document.createElement('div');
+
+newElement.setAttribute('user-id', 1);
+newElement.classList.add('panel');
+newElement.innerHTML = `<button class="${buttonClass}">${buttonText}</button>`;
+
+document.querySelector('.test').appendChild(newElement);
